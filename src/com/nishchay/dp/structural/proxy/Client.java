5717c605ecb808779f263caef25e508ffceccc41
nnares <nishchay.naresh@gmail.com>
2020-12-01 19:46:08 +0530

    +u proxy.Client.java

--- src/com/nishchay/dp/structural/proxy/Client.java
+++ src/com/nishchay/dp/structural/proxy/Client.java
@@ -1,13 +1,13 @@
 package com.nishchay.dp.structural.proxy;
 
-public class Client
-{
-    public static void main(String[] args) 
-    {
+public class Client {
+
+    public static void main(String[] args) {
         // creating an proxy object for an real object (which does some functionality)
         // client is invoking real object functionality over proxy object, which in turn gets executed by real Object
 
         RealObject proxy = new RealObjectProxy();
         proxy.doSomething();
     }
+
 }
