7fe97f88378bf3dfbcad751613875c6e0dfcdb4c
nnares <nishchay.naresh@gmail.com>
2020-12-01 19:19:35 +0530

    +u facade.Driver.java

--- src/com/nishchay/dp/structural/facade/Driver.java
+++ src/com/nishchay/dp/structural/facade/Driver.java
@@ -4,6 +4,7 @@ public class Driver {
 
   public static void main(String[] args) {
     Facade facade = new Facade();
+    // here facade object is good to perform all available functionality based on client request
     facade.flightBook();
     facade.hotelBook();
     facade.holidayBook();
