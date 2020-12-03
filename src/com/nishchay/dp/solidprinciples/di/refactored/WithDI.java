4e7c9b78d8c6eb6b533021596a94a81cae773fc1
nnares <nishchay.naresh@gmail.com>
2020-12-04 00:44:46 +0530

    + solidprinciples.di

---
+++ src/com/nishchay/dp/solidprinciples/di/refactored/WithDI.java
@@ -0,0 +1,62 @@
+package com.nishchay.dp.solidprinciples.di.refactored;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * The outcome is that the Project class does not depend on lower level modules, but rather abstractions.
+ * Also, low-level modules and their details depend on abstractions.
+ *
+ * */
+public class WithDI {
+
+    public static void main(String[] args) {
+
+        List<Developer> developers = new ArrayList<>();
+        developers.add(new BackEndDeveloper());
+        developers.add(new FrontEndDeveloper());
+
+        Project project = new Project(developers);
+        project.implement();
+    }
+}
+
+interface Developer {
+    void develop();
+}
+
+class BackEndDeveloper implements Developer {
+    @Override
+    public void develop() {
+        writeJava();
+    }
+
+    public void writeJava() {
+        System.out.println("java code");
+    }
+}
+
+class FrontEndDeveloper implements Developer {
+    @Override
+    public void develop() {
+        writeJavascript();
+    }
+
+    public void writeJavascript() {
+        System.out.println("javascript code");
+    }
+}
+
+class Project {
+
+    private List<Developer> developers;
+
+    public Project(List<Developer> developers) {
+        this.developers = developers;
+    }
+
+    public void implement() {
+        developers.forEach(d -> d.develop());
+    }
+
+}
