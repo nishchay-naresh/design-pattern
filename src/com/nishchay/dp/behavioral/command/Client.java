7dc761b78b5d0cf7ca0fe625370082c746f8252e
nnares <nishchay.naresh@gmail.com>
2020-12-21 00:09:56 +0530

    + behavioral.command

--- src/com/nishchay/dp/behavioral/command/Client.java
+++ src/com/nishchay/dp/behavioral/command/Client.java
@@ -5,13 +5,13 @@ public class Client {
     public static void main(String[] args) {
 
         /*
-        *
-        Flow will be like that =>
-            1.	create a concrete command object
-            2.	set its receiver in it (means its processor)
-            3.	set this concrete command object to invoker
-            4.	then do invoker.execute()
-        * */
+         *
+         * Flow will be like that =>
+         *    1.	create a concrete command object
+         *    2.	set its receiver in it (means its processor)
+         *    3.	set this concrete command object to invoker
+         *    4.	then do invoker.execute()
+         * */
         IReceiver receiver = new WindowsFileReceiver();
         ICommand command = new OpenCommand(receiver);
         Invoker invoker = new Invoker(command);
