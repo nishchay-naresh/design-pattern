11a0c8d891a65a35b181819f6d86211b14f2de8a
nnares <nishchay.naresh@gmail.com>
2020-12-21 00:07:10 +0530

    + behavioral.command

---
+++ src/com/nishchay/dp/behavioral/command/Client.java
@@ -0,0 +1,32 @@
+package com.nishchay.dp.behavioral.command;
+
+public class Client {
+
+    public static void main(String args[]) {
+
+        /*
+        *
+        Flow will be like that =>
+            1.	create a concrete command object
+            2.	set its receiver in it (means its processor)
+            3.	set this concrete command object to invoker
+            4.	then do invoker.execute()
+        * */
+        IReceiver receiver = new WindowsFileReceiver();
+        ICommand command = new OpenCommand(receiver);
+        Invoker invoker = new Invoker(command);
+        invoker.execute();
+
+        receiver = new UnixFileReceiver();
+        command = new OpenCommand(receiver);
+        invoker = new Invoker(command);
+        invoker.execute();
+
+        receiver = new UnixFileReceiver();
+        command = new CloseCommand(receiver);
+        invoker = new Invoker(command);
+        invoker.execute();
+
+
+    }
+}
