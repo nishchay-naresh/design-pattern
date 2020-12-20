46ccde50b9b27ef242e9b0608e34a8cebab545d1
nnares <nishchay.naresh@gmail.com>
2020-12-21 00:08:32 +0530

    + behavorial.strategy

--- src/com/nishchay/dp/behavioral/command/Client.java
+++ src/com/nishchay/dp/behavioral/command/Client.java
@@ -2,7 +2,7 @@ package com.nishchay.dp.behavioral.command;
 
 public class Client {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         /*
         *
