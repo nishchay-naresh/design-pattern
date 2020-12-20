33c5ca1360a1b21df5c316e2951578f220b83200
nnares <nishchay.naresh@gmail.com>
2020-12-20 23:36:57 +0530

    + behavorial.strategy

---
+++ src/com/nishchay/dp/behavorial/strategy/CardStrategy.java
@@ -0,0 +1,16 @@
+package com.nishchay.dp.behavorial.strategy;
+
+public class CardStrategy implements PaymentStrategy {
+
+	private String cardNumber;
+
+	public CardStrategy(String ccNum){
+		this.cardNumber=ccNum;
+	}
+
+	@Override
+	public void pay(int amount) {
+		System.out.printf("\nRs %d paid with Card - %s", amount, cardNumber);
+	}
+
+}
