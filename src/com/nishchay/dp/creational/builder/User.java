3d34a96f1d4480f7e9970ffd9634af1116490e88
nnares <nishchay.naresh@gmail.com>
2021-05-02 13:16:47 +0530

    + formatting

--- src/com/nishchay/dp/creational/builder/User.java
+++ src/com/nishchay/dp/creational/builder/User.java
@@ -1,18 +1,18 @@
 package com.nishchay.dp.creational.builder;
 /*
-* Spring boot User class is the another good example where builder has been used to get the immutable copy of complex User class
-* https://docs.spring.io/spring-security/site/docs/4.2.15.RELEASE/apidocs/org/springframework/security/core/userdetails/User.UserBuilder.html
-* */
+ * Spring boot User class is the another good example where builder has been used to get the immutable copy of complex User class
+ * https://docs.spring.io/spring-security/site/docs/4.2.15.RELEASE/apidocs/org/springframework/security/core/userdetails/User.UserBuilder.html
+ *
+ * */
 
-public class User
-{
+public class User {
     //All final attributes
     private final String firstName; // required
     private final String lastName; // required
     private final int age; // optional
     private final String phone; // optional
     private final String city; // optional
- 
+
     private User(UserBuilder builder) {
         this.firstName = builder.firstName;
         this.lastName = builder.lastName;
@@ -20,59 +20,67 @@ public class User
         this.phone = builder.phone;
         this.city = builder.city;
     }
- 
+
     //All getter, and NO setter to provide immutability
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public int getAge() {
         return age;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public String getCity() {
         return city;
     }
- 
+
     @Override
     public String toString() {
-        return "User: "+this.firstName+", "+this.lastName+", "+this.age+", "+this.phone+", "+this.city;
+        return "User: " + this.firstName + ", " + this.lastName + ", " + this.age + ", " + this.phone + ", " + this.city;
     }
- 
-    public static class UserBuilder 
-    {
+
+    public static class UserBuilder {
         private final String firstName;
         private final String lastName;
         private int age;
         private String phone;
         private String city;
- 
+
         public UserBuilder(String firstName, String lastName) {
             this.firstName = firstName;
             this.lastName = lastName;
         }
+
         public UserBuilder age(int age) {
             this.age = age;
             return this;
         }
+
         public UserBuilder phone(String phone) {
             this.phone = phone;
             return this;
         }
+
         public UserBuilder city(String city) {
             this.city = city;
             return this;
         }
+
         //Return the finally constructed User object
         public User build() {
-            User user =  new User(this);
+            User user = new User(this);
             validateUserObject(user);
             return user;
         }
+
         private void validateUserObject(User user) {
             //Do some basic validations to check 
             //if user object does not break any assumption of system
