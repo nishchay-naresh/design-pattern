717c8dc43d442e6b852e80b62a5026d4e2557dfa
nnares <nishchay.naresh@gmail.com>
2022-05-20 11:27:17 +0530

    +u dp.structural.decorator.Driver.java

--- src/com/nishchay/dp/structural/decorator/Driver.java
+++ src/com/nishchay/dp/structural/decorator/Driver.java
@@ -1,9 +1,23 @@
 package com.nishchay.dp.structural.decorator;
 
+import com.nishchay.dp.structural.decorator.entity.Circle;
+import com.nishchay.dp.structural.decorator.entity.Shape;
+import com.nishchay.dp.structural.decorator.features.BlueAreaShapeDecorator;
+import com.nishchay.dp.structural.decorator.features.GreenShapeDecorator;
+/*
+* FileReader and BufferedReader are examples of decorator
+*
+* Where ever we are doing any feature enhancement for any entity - doing same set of things + sth extra
+* 	checkedXxx() : checkedCollection(), checkedList(), checkedMap()
+* 	synchronizedXxx() : synchronizedList(), synchronizedMap(), synchronizedSet()
+* 	unmodifiableXxx() : unmodifiableSet(), unmodifiableMap() and unmodifiableList()
+* methods decorate the received object and return the same.
+*
+* */
 public class Driver {
 
   public static void main(String[] args) {
-    // FileReader and BufferedReader are examples of decorator
+
     Circle circle = new Circle();
     System.out.println("Original object behaviour -");
     circle.draw();
