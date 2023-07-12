fb5a0c862a6cac8a9e847ed2d9cb36c66417e74f
Nishchay <nishchay.naresh@gmail.com>
2023-07-12 14:12:29 +0530

    +u dp.creational.factory.Factory.java

--- src/com/nishchay/dp/creational/factory/Factory.java
+++ src/com/nishchay/dp/creational/factory/Factory.java
@@ -2,21 +2,39 @@ package com.nishchay.dp.creational.factory;
 
 import com.nishchay.dp.creational.factory.parse.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class Factory {
 
-    public static Parser getParser(String parserType) {
-        Parser parser = null;
-        if (parserType.equalsIgnoreCase("XmlParser"))
-            parser = new XmlParser();
-        else if (parserType.equalsIgnoreCase("JsonParser"))
-            parser = new JsonParser();
-        else if (parserType.equalsIgnoreCase("FixedLengthParser"))
-            parser = new FixedLengthParser();
-        else if (parserType.equalsIgnoreCase("ProtBuffParser"))
-            parser = new ProtBuffParser();
-        else
-            throw new IllegalArgumentException("No such parser exists");
-        return parser;
+    public static Parser createParser(String parserType) {
+
+        switch (parserType) {
+            case "xml":
+                return new XmlParser();
+            case "json":
+                return new JsonParser();
+            case "protbuff":
+                return new ProtBuffParser();
+            case "fixedlength":
+                return new FixedLengthParser();
+            default:
+                throw new IllegalArgumentException("No such parser exists" + parserType);
+        }
     }
 
+    final static Map<String, Supplier<Parser>> map = new HashMap<>();
+    static {
+        map.put("xml", XmlParser::new);
+        map.put("json", JsonParser::new);
+        map.put("protbuff", ProtBuffParser::new);
+        map.put("fixedlength", FixedLengthParser::new);
+    }
+    public static Parser createParserJava8(String parserType) {
+        Supplier<Parser> parserSupplier = map.get(parserType);
+        if(parserSupplier != null)
+            return parserSupplier.get();
+        throw new IllegalArgumentException("No such parser exists" + parserType);
+    }
 }
