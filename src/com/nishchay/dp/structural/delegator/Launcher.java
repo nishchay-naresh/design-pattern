2cfe45fe35bd02f50d7f5f0002df572e61a7ec53
Nishchay <nishchay.naresh@gmail.com>
2023-10-05 09:27:24 +0530

    +u dp.structural.delegator.Launcher.java

--- src/com/nishchay/dp/structural/delegator/Launcher.java
+++ src/com/nishchay/dp/structural/delegator/Launcher.java
@@ -9,7 +9,8 @@ public class Launcher {
         hardCodedEnrichmentEx();
         System.out.println("-----------------------------");
         unaryOperatorEnrichmentEx();
-
+        System.out.println("-----------------------------");
+        delegationUsingFn();
     }
 
     private static void hardCodedEnrichmentEx() {
@@ -31,13 +32,17 @@ public class Launcher {
     /**
      * This is a generic methods, takes below two params, and then later doing the things for enriched version of object
      *
-     * @param baseVersion
-     * @param enrichmentLogic
+     * @param baseVersion DefaultTaskImpl
+     * @param enrichmentLogic UnaryOperator<Task>
      */
     private static void doingEnrichmentThing(DefaultTaskImpl baseVersion, UnaryOperator<Task> enrichmentLogic) {
         Task enrichedVersion = enrichmentLogic.apply(baseVersion);
         doingTasks(enrichedVersion);
     }
 
-
+    private static void delegationUsingFn() {
+        Task delegatedTask = new DelegatedTaskFn();
+        delegatedTask.doTask1();
+        delegatedTask.doTask2();
+    }
 }
